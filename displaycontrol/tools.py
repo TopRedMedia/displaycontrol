4a4389ee065d3e7d2600c71960ffba4e8f9a5325
Mirko Haaser <mirko@topred.media>
2018-08-21 11:19:53 +0200

    os independent detection of serial ports.

--- displaycontrol/tools.py
+++ displaycontrol/tools.py
@@ -1,8 +1,11 @@
-import json, struct
-import os
+import glob
+import serial
+import struct
+import sys
+
+
 # import win32service
 # import win32serviceutil
-import serial
 
 
 class Tools:
@@ -33,14 +36,27 @@ class Tools:
 
     @staticmethod
     def get_available_comports():
-        available = []
-        ports = ['COM%s' % (i + 1) for i in range(10)]
+        """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+        """
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+        result = []
         for port in ports:
             try:
-                ser = serial.Serial(port)
-                ser.close()
-                available.append(port)
-            except Exception, err:
-                ser.close()
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except Exception:
                 pass
-        return available
+        return result
